744f828ef8dc84348c9491915ec1d3645819b047
Jatindarji03 <djatin292@gmail.com>
2026-04-28 22:58:17 +0530

    fix:cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://apitestingtool.netlify.app/workspace",
+    "https://apitestingtool.netlify.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
